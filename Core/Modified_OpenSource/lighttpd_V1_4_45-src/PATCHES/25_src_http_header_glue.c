e4bcbfa346f80aa77a46b6e67a5fd6ebc054047f
ShanWunYang <shanwunyang@ami.com>
2023-08-11 11:55:21 +0800

    Weekly Update 2023.08.11

--- Core/Modified_OpenSource/lighttpd_V1_4_45-src/PATCHES/25_src_http_header_glue.c
+++ Core/Modified_OpenSource/lighttpd_V1_4_45-src/PATCHES/25_src_http_header_glue.c
@@ -1,18 +1,18 @@
---- lighttpd-1.4.45/src/http-header-glue.c	2023-05-12 16:20:36.678593400 +0530
-+++ lighttpd_new/src/http-header-glue.c	2023-05-12 16:20:36.682593398 +0530
-@@ -663,7 +663,14 @@
+--- lighttpd-1.4.45/src/http-header-glue.c	2017-01-14 10:35:17.000000000 +0530
++++ lighttpd_new/src/http-header-glue.c		2023-07-26 19:36:42.498862239 +0530
+@@ -663,7 +663,15 @@
  	 * the HEAD request will drop it afterwards again
  	 */
  	if (0 == sce->st.st_size || 0 == http_chunk_append_file(srv, con, path)) {
--		con->http_status = 200;
-+                if((con->request.http_method == 2) && (con->request.content_length <= 0))
-+                {
-+                        con->http_status = 400;
-+                }
-+                else
-+                {
-+                        con->http_status = 200;
-+                }
++		if((con->request.http_method == 2) && (con->request.content_length <= 0))
++		{
++			con->http_status = 400;
++			con->keep_alive = 0;
++		}
++		else
++		{
+ 		con->http_status = 200;
++		}
  		con->file_finished = 1;
  	} else {
  		con->http_status = 403;
